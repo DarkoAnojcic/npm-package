5ab4d77abc8c6433edf6b0ed881152e1eeb08b44
Darko Anojcic <darko.anojcic@gmail.com>
2023-09-05 15:41:59 +0200

    fix(index.js): small change

--- src/index.js
+++ src/index.js
@@ -1,46 +1,58 @@
-'use strict';
+// 'use strict';
 
-const fs = require('fs');
+// const fs = require('fs');
 
-let rawdata = fs.readFileSync('src/cucumber_report.json');
-let data = JSON.parse(rawdata);
+// let rawdata = fs.readFileSync('src/cucumber_report.json');
+// let data = JSON.parse(rawdata);
 
-const request = [];
-let title;
-let steps = [];
-let scenarios = [];
-data.forEach(element => {
-    scenarios = [];
-    // console.log(element.name);
-    element.elements.forEach(scenario => {
-        title = scenario.name;
-        steps = [];
-        // console.log(scenario.name);
-        scenario.steps?.forEach(step => {
-            if(step.name !== undefined) {
-                steps.push({
-                    step: step.keyword + step.name,
-                    status: step.result.status,
-                    duration: step.result.duration,
-                    error_message: step.result.error_message
-                })
-                // console.log(step.keyword + step.name);
-            }
-        });
-        scenarios.push({
-            title,
-            steps
-            }
-        );
-        // console.log(element.name);
-        // console.log(scenario.name);
-    });
+// const request = [];
+// let title;
+// let steps = [];
+// let scenarios = [];
+// data.forEach(element => {
+//     scenarios = [];
+//     // console.log(element.name);
+//     element.elements.forEach(scenario => {
+//         title = scenario.name;
+//         steps = [];
+//         // console.log(scenario.name);
+//         scenario.steps?.forEach(step => {
+//             if(step.name !== undefined) {
+//                 steps.push({
+//                     step: step.keyword + step.name,
+//                     status: step.result.status,
+//                     duration: step.result.duration,
+//                     error_message: step.result.error_message
+//                 })
+//                 // console.log(step.keyword + step.name);
+//             }
+//         });
+//         scenarios.push({
+//             title,
+//             steps
+//             }
+//         );
+//         // console.log(element.name);
+//         // console.log(scenario.name);
+//     });
 
-    request.push({
-        feature: element.name,
-        scenarios
-    }
-    )
-});
+//     request.push({
+//         feature: element.name,
+//         scenarios
+//     }
+//     )
+// });
 
-console.log(JSON.stringify(request));
+// console.log(JSON.stringify(request));
+
+"use strict";
+
+import {packageDirectory} from 'pkg-dir';
+
+async function getPath() {
+    console.log(await packageDirectory());
+}
+
+module.exports = {
+    path: getPath
+}
